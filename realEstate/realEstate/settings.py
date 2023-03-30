2435096baa7d22a5190569c7a9695954d3176a85
Kibe Stevie <101211069+Dispenser254@users.noreply.github.com>
2023-03-30 12:24:54 +0300

    Update settings.py

--- realEstate/realEstate/settings.py
+++ realEstate/realEstate/settings.py
@@ -147,8 +147,8 @@ AUTH_USER_MODEL = 'accounts.User'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT ='587'
-EMAIL_HOST_USER ='kibestevie02@gmail.com'
-EMAIL_HOST_PASSWORD='gccycblmiflmhhma'
+EMAIL_HOST_USER ='' # Enter your email address
+EMAIL_HOST_PASSWORD='' # Enter your email password
 EMAIL_USE_TLS ='True'
 
 # Default primary key field type
